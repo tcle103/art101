82ffa862d27ce23a1532327a5771f7e65f8fd772
tcle103 <146970839+tcle103@users.noreply.github.com>
2023-11-02 18:24:28 -0700

    Lab 7 so far
    created lab7 directory with required files, added some screenshots that I generated while working on lab.js
    
    lab. js changes - implemented sortName() and removeSpaces() for Task X 1 and 2; began nameArrayToString()

---
+++ lab7/js/lab.js
@@ -0,0 +1,107 @@
+// Lab 7 - Functions
+// Tien Le <ticale@ucsc.edu>
+//11/3/23
+
+//Function declarations
+
+/* sortName(name)
+    Sorts the characters representing in a name by alphabetical order
+    @param: name, a string
+    @return: sorted lowercase array
+*/
+function sortName(name) {
+    // Task X 1: pretty easy if you make all lowercase, hard part is making them print uppercase (dunno if that matters)
+    // https://www.w3schools.com/jsref/jsref_toLowerCase.asp ; toLowerCase() is NOT in place   
+    nameArray = name.toLowerCase().split(''); // Create an array of only lowercased characters
+    
+    removeSpaces(nameArray); // Remove spaces from array, in place means don't have to assign it to anything
+    // does it matter that i've called this here before declaring it? - tested in console, the answer is no!
+    
+    nameArray.sort(); // sort in place
+
+    return nameArray; // it's not quite what the lab calls for but for Task X it's more useful to return the array as is
+}
+
+/* removeSpaces(array)
+    Helper function for removing all the characters representing spaces from an array in place
+    @param: array, an array of string tokens
+    @return: array without spaces
+*/
+function removeSpaces(array) {
+    // Task X 2: Help with removing spaces - https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array-in-javascript
+    var i = 0; // counter
+    while (i < array.length) { 
+        if (array[i] === ' ') {
+            array.splice(i, 1);
+        }
+        else { // this one is kind of tricky but i think it's cause this index will contain the next item if spliced
+            ++i; // didn't know there was c style increment here
+        }
+    }
+    return;
+}
+
+/* nameArrayToString(name, nameArray)
+    Turns a nameArray provided by sortName to a displayable string form, restoring capitalization
+    @param: name, the original name string; nameArray, an array of sorted lowercase string tokens
+    @return: finalString, single string w/ capitalization
+*/
+function nameArrayToString(name, nameArray) {
+    // Part of Task X 1: shows sorted correctly WITH uppercase
+    // String iteration help here: https://stackoverflow.com/questions/5943726/string-charatx-or-stringx/5943807#5943807
+    // I'm gonna use bracket notation since I doubt anybody will run this on IE7...
+    
+    // Make a copy of nameArray, initialize other variables
+    var copyArray = nameArray.slice();
+    var arraySlice = [];
+    var finalString = "";
+
+    while (copyArray.length != 0) {
+        // create slices of duplicate characters, reducing number of characters left in copyArray
+        if (copyArray.length > 1) { 
+            var i = 0; // counter
+            while (i < (copyArray.length-1)) {
+                if (copyArray[i] != copyArray[i+1]) { // compare with next elem in array, if differs, create slice of only duplicates
+                    arraySlice = copyArray.slice(0, i+1);
+                    copyArray.splice(0, arraySlice.length); // remove sliced portion from copyArray
+                    console.log(arraySlice, copyArray);
+                    break;
+                }
+                else {
+                    if ((i+1) === (copyArray.length-1)) { // in case it's duplicates until the end of the array
+                        arraySlice = copyArray.slice();
+                        copyArray.splice(0); // ensure copyArray is empty now!
+                        console.log(arraySlice, copyArray);
+                        break;
+                    }
+                }
+                ++i;
+            }
+        }
+        else { // only one elem left in copyArray
+            arraySlice = copyArray.slice();
+                copyArray.splice(0); // ensure copyArray is empty now!
+                console.log(arraySlice, copyArray);
+        }
+        
+        // count how many capitals of each character is in name
+        
+    }
+    return;
+}
+
+
+
+// Task X 3: Shuffling, see if there is a random for use in JS.
+// Will want to write a function for random picking from an array instead of in a loop since the array size will decrease constantly
+// https://www.w3schools.com/JS/js_random.asp
+
+// Task X 4: Simply include your <div> tag in your document.writeln() line (works for <i> etc.)
+
+/* Task X 5: pretty easy if you just declare your name var at beginning and then call function with that param
+
+Some weirdness for Task X 5: 
+function printName(name = window.prompt("Enter your name:")) {
+    console.log(name);
+}
+*/
