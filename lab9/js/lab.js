cbd2ba6a5eb4662597c5cb8d00851d4dc09a766c
tcle103 <146970839+tcle103@users.noreply.github.com>
2023-11-11 03:07:20 -0800

    Lab 9
    Completed all the prerequisite stuff for Lab 9  and linked from homepage

---
+++ lab9/js/lab.js
@@ -0,0 +1,31 @@
+// Lab 9 - Libraries and jQueries
+// Tien Le <ticale@ucsc.edu>
+// 11/11/23
+
+// make buttons in each sections
+$(".minor-section").append("<button class='special-button'>Make Special</button>");
+
+// make each button toggle "special" for the section it is in on
+$(".special-button").click(function(){ 
+    $(this).parent().toggleClass("special");
+})
+
+// longer version
+/*
+// make buttons in each section
+$("#challenge").append("<button id='challenge-button'>Make Special</button>");
+$("#thoughts").append("<button id='thoughts-button'>Make Special</button>");
+$("#results").append("<button id='results-button'>Make Special</button>");
+
+// make each button toggle "special" for its section
+$("#challenge-button").click(function(){
+    $("#challenge-button").toggleClass("special");
+});
+$("#thoughts-button").click(function(){
+    $("#thoughts-button").toggleClass("special");
+});
+$("#results-button").click(function(){
+    $("#results-button").toggleClass("special");
+});
+*/
+
